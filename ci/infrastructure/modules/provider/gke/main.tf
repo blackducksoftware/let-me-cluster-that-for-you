a664f8ff695d9e1b197184492313325edcb00bf8
Yash Bhutwala <ymb002@bucknell.edu>
2019-12-13 11:12:15 -0500

    update and pin dependency versions

--- ci/infrastructure/modules/provider/gke/main.tf
+++ ci/infrastructure/modules/provider/gke/main.tf
@@ -15,7 +15,7 @@ module "gke" {
 
 module "gcp-network" {
   source  = "terraform-google-modules/network/google"
-  version = "~> 1.4.0"
+  version = "~> 1.4.3"
 
   project_id   = var.project_id
   network_name = var.network
--- ci/infrastructure/modules/provider/gke/versions.tf
+++ ci/infrastructure/modules/provider/gke/versions.tf
@@ -1,6 +1,9 @@
 terraform {
   required_version = ">= 0.12"
   required_providers {
-    google = "~> 2.18"
+    google = "~> 2.20"
+    null = "~> 2.1"
+    random = "~> 2.2"
+    kubernetes = "~> 1.10"
   }
 }

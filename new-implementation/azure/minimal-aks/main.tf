2c3697bdf7060c78280f6099d3a8016f42724a1a
Yash Bhutwala <ymb002@bucknell.edu>
2020-01-20 11:53:48 -0500

    cleanup aks, add more docs

--- new-implementation/azure/minimal-aks/main.tf
+++ new-implementation/azure/minimal-aks/main.tf
@@ -3,7 +3,6 @@ resource "azurerm_resource_group" "main" {
   location = var.location
 }
 
-
 module "aks" {
   source  = "Azure/aks/azurerm"
   version = "2.0.0"
@@ -14,33 +13,3 @@ module "aks" {
   location = var.location
   kubernetes_version = var.kubernetes_version
 }
-
-// resource "random_string" "suffix" {
-//   length  = 4
-//   special = false
-// }
-// locals {
-//     /* Local variables */
-//   name = "${var.cluster_name}-${random_string.suffix.result}"
-// }
-// 
-// module "eks-public" {
-//     source = "../backends/eks"
-//     cluster_name = "${local.name}"
-//     kubernetes_version = var.kubernetes_version
-//     region = var.region
-//     workers = var.workers
-//     instance_type = var.instance_type
-// }
-// 
-// module "postgres-rds" {
-//     source =  "../backends/postgres-rds"
-//     region = var.region
-//     db_name = var.cluster_name
-//     db_username = var.db_username
-//     db_password = var.db_password
-//     postgres_version = var.postgres_version
-//     security_groups = "${module.eks-public.worker_node_security_group_id}"
-//     subnets = "${module.eks-public.vpc_database_subnets}"
-//     vpc_id = "${module.eks-public.vpc_id}"
-// }

ae7c87b1afd7ea02b8b98af4b18b09b87447a041
Vasiliy Sharapov <vasiliys@gmail.com>
2020-01-27 11:18:36 -0500

    AKS with PG

--- new-implementation/azure/backends/aks-by-terraform-providers/outputs.tf
+++ new-implementation/azure/backends/aks-by-terraform-providers/outputs.tf
@@ -2,3 +2,7 @@ output "kubeconfig_path" {
   value = local_file.kubeconfig.filename
 }
 
+output "rg_name" {
+  value = azurerm_resource_group.tf-k8s-acc.name
+
+}
---
+++ new-implementation/azure/backends/azure-db/variables.tf
@@ -0,0 +1,45 @@
+variable "rg_name" {
+  description = "resource group name"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be provisioned"
+}
+
+variable "pg_server_name" {
+  default = "pg-server"
+  description = "cannot be empty or null. It can only be made up of lowercase letters 'a'-'z', the numbers 0-9 and the hyphen. The hyphen may not lead or trail in the name."
+}
+
+variable "pg_sku" {
+  default = "B_Gen5_2"
+}
+
+variable "storage_mb" {
+  default = 5120
+}
+
+variable "backup_retention_days" {
+  default = 7
+}
+
+variable "geo_redundant_backup" {
+  default = "Disabled"
+}
+
+variable "administrator_login" {
+  default = "psqladminun"
+}
+
+variable "administrator_login_password" {
+  default = "H@Sh1CoR3!"
+}
+
+variable "pg_version" {
+  default = "9.6"
+}
+
+variable "ssl_enforcement" {
+  default = "Enabled"
+}
+

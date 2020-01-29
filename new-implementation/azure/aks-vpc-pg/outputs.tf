191d63a8e62bacc808ed03c4d390bb1e2a8e4d4e
Vasiliy Sharapov <vasiliys@gmail.com>
2020-01-29 16:23:00 -0500

    Azure random pass (#23)
    * Added a better AKS module, with VPC
    
    * Main module outputs "psql_user_pass"
    
    Which is randomly generated in azure-db module

--- new-implementation/azure/aks-vpc-pg/outputs.tf
+++ new-implementation/azure/aks-vpc-pg/outputs.tf
@@ -3,15 +3,12 @@ output "kubeconfig_path" {
   value = module.aks-with-vpc.kubeconfig_path
 }
 
+output "psql_user_pass" {
+  value = module.db-on-vpc.password
+}
+
 output "psql_conn" {
  description = "The connection name of the master instance to be used in connection strings"
  sensitive   = true
  value       = module.db-on-vpc.fqdn
 }
-
-# TODO: generate a random password; do that in submodule too
-#output "psql_user_pass" {
-#  description = "The password for the default user. If not set, a random one will be generated and available in the generated_user_password output variable."
-#  sensitive   = true
-#  value       = module.custom_postgresql_db.psql_user_pass
-#}
--- new-implementation/azure/backends/azure-db/outputs.tf
+++ new-implementation/azure/backends/azure-db/outputs.tf
@@ -2,6 +2,10 @@ output "pg_server_name" {
   value = azurerm_postgresql_server.pg_server.name
 }
 
+output "password" {
+  value = azurerm_postgresql_server.pg_server.administrator_login_password
+}
+
 output "fqdn" {
   value = azurerm_postgresql_server.fqdn
 }

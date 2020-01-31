fd01fbf79e924c831c56e3cc47d037b440f69bb3
Yash Bhutwala <ymb002@bucknell.edu>
2020-01-31 11:31:45 -0500

    update the jenkins files

--- new-implementation/letMeClusterThatForYou.groovy
+++ new-implementation/letMeClusterThatForYou.groovy
@@ -12,9 +12,10 @@ pipeline {
                         echo "$cloud_provider $cluster_name $kubernetes_version $postgresql_version"
                         if ("${cloud_provider}" == "GKE") {
                             sh '''
-                                pwd
                                 cat ${INFRA} > infra.json
-                                docker run --name $cluster_name -v $(pwd):/root/creds -v /var/lib/jenkins/kubeconfigs:/opt/kubeconfigs -v /home/bhutwala/.ssh:/root/.ssh ybhutdocker/lmctfy:google sh -c "cd /lmctfy/google/public-gke-private-postgres-cloudsql; terraform init; export GOOGLE_APPLICATION_CREDENTIALS="/root/creds/infra.json" && terraform apply --auto-approve -var="project_id=snps-polaris-onprem-dev" -var="cluster_name=$cluster_name" -var="network_name=$cluster_name" -var="subnet_name=$cluster_name";cp /lmctfy/google/backends/gke-with-vpc/kubeconfig /opt/kubeconfigs/$cluster_name-config" || docker commit $cluster_name $cluster_name-lmctfy:$cluster_name
+                                docker run --name ${cluster_name} -v $(pwd):/root/creds -v /var/lib/jenkins/kubeconfigs:/opt/kubeconfigs -v /var/lib/jenkins/tfstate:/opt/tfstate -v /home/bhutwala/.ssh:/root/.ssh ybhutdocker/lmctfy:google sh -c "cd /lmctfy/google/public-gke-private-postgres-cloudsql; terraform init; export GOOGLE_APPLICATION_CREDENTIALS=\\"/root/creds/infra.json\\" && terraform apply --auto-approve -var=\\"project_id=snps-polaris-onprem-dev\\" -var=\\"cluster_name=${cluster_name}\\" -var=\\"network_name=${cluster_name}\\" -var=\\"subnet_name=${cluster_name}\\" ; cp terraform.tfstate /opt/tfstate/${cluster_name}-terraform.tfstate; cp \\$(terraform output kubeconfig_path) /opt/kubeconfigs/${cluster_name}-config " || docker commit ${cluster_name} ${cluster_name}-jenkins-lmctfy:${cluster_name}
+
+                                docker commit ${cluster_name} ${cluster_name}-lmctfy:${cluster_name}
                             '''
                         }
                     }

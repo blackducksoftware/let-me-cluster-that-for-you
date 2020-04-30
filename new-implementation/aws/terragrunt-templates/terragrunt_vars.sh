b1a14a32cc5406b4b8e89c0e52598e3fefc9b7e2
sripathi <sripathi@synopsys.com>
2020-04-30 14:22:02 +0530

    changed eks version

--- new-implementation/aws/terragrunt-templates/terragrunt_vars.sh
+++ new-implementation/aws/terragrunt-templates/terragrunt_vars.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
 export TF_VAR_region="ap-south-1"
-export TF_VAR_kubernetes_version="1.14"
-export TF_VAR_cluster_name="onpremreportingeks"
+export TF_VAR_kubernetes_version="1.15"
+export TF_VAR_cluster_name="onprem-reporting"
 
 export TF_VAR_db_name="onpremreportingdb"
 export TF_VAR_db_username="postgres"
 export TF_VAR_db_password="postgres"
 export TF_VAR_postgres_version="9.6"
-
-
-
-
-
 

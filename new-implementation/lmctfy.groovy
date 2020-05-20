ae8b2597bd4de382c8a66232a6ee9ad192410413
sripathi <sripathi@synopsys.com>
2020-05-20 14:08:43 +0530

    fixed template sname

--- new-implementation/lmctfy.groovy
+++ new-implementation/lmctfy.groovy
@@ -59,9 +59,9 @@ pipeline {
                                export terragrunt_template="reporting-vnet-aks"
                            elif [[ $external_db_required == "Yes" && $provsion_env_for == "Blackduck_Hub" ]]
                            then
-                               export terragrunt_template="blackduck-vpc-postgres-eks"
+                               export terragrunt_template="blackduck-vnet-postgres-aks"
                            else
-                               export terragrunt_template="blackduck-vpc-postgres-eks"
+                               export terragrunt_template="blackduck-vnet-aks"
                            fi
                            export GOOGLE_APPLICATION_CREDENTIALS="${infra_key}"
                            export TF_VAR_aks_client_secret=$AZURE_CLIENT_SECRET
@@ -105,9 +105,9 @@ pipeline {
                                export terragrunt_template="reporting-vpc-gke"
                            elif [[ $external_db_required == "Yes" && $provsion_env_for == "Blackduck_Hub" ]]
                            then
-                               export terragrunt_template="blackduck-vpc-postgres-eks"
+                               export terragrunt_template="blackduck-vpc-cloudsql-gke"
                            else
-                               export terragrunt_template="blackduck-vpc-postgres-eks"
+                               export terragrunt_template="blackduck-vpc-gke"
                            fi
                            export GOOGLE_APPLICATION_CREDENTIALS="${infra_key}"
                            network_name=$(echo "$cluster_name" | tr '[:upper:]' '[:lower:]')"-network"
